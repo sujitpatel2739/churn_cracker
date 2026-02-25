a45afa00a4d7d905360362ea0cea02c71e5d2163
Sujit Patel <202501987+sujitpatel2739@users.noreply.github.com>
2026-02-25 23:13:50 +0530

    Initialized to train the model

--- models/xgboost.py
+++ models/xgboost.py
@@ -10,4 +10,20 @@ y_train = train_df['churn_label']
 X_test = test_df.drop(columns=['customer_id', 'churn_label'])
 y_test = test_df['churn_label']
 
-# model = XGBClassifier.
+
+# Initializing and training the model
+model = XGBClassifier(
+    n_estimators=200,
+    max_depth=6,
+    learning_rate=0.1,
+    subsample=0.8,
+    colsample_bytree=0.8,
+    objective='binary:logistic',
+    eval_metric='logloss',
+    random_state=42
+)
+
+model.fit(X_train, y_train,
+          early_stopping_rounds=20,
+          verbose=True)
+

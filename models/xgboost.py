d8fbf305f9a727503882328a07e69573e20c89b5
Sujit Patel <202501987+sujitpatel2739@users.noreply.github.com>
2026-02-25 23:11:28 +0530

    created X an y data for train and test df

--- models/xgboost.py
+++ models/xgboost.py
@@ -4,3 +4,10 @@ from features.splitting import train_df, test_df
 from xgboost import XGBClassifier
 from sklearn.metrics import roc_auc_score, precision_score
 
+# Drop the labels and customer_id columns from train_df and test df
+X_train = train_df.drop(columns=['customer_id', 'churn_label'])
+y_train = train_df['churn_label']
+X_test = test_df.drop(columns=['customer_id', 'churn_label'])
+y_test = test_df['churn_label']
+
+# model = XGBClassifier.

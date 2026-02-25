ee083cdf6844983ce93b3b725be0e8c5dbf27be5
Sujit Patel <202501987+sujitpatel2739@users.noreply.github.com>
2026-02-25 23:31:31 +0530

    Evaluating model on Precision@TopK(K=10%)

--- models/xgboost.py
+++ models/xgboost.py
@@ -1,5 +1,5 @@
 from features.splitting import train_df, test_df
-
+import numpy as np
 
 from xgboost import XGBClassifier
 from sklearn.metrics import roc_auc_score, precision_score
@@ -27,6 +27,8 @@ model.fit(X_train, y_train,
           early_stopping_rounds=20,
           verbose=True)
 
+model.save_model('xgboost.xgb')
+
 # Evaluating the trained model on AUC-ROC curve and Precision@Top10%
 model = XGBClassifier()
 model.load_model('xgboost.xgb')
@@ -40,3 +42,13 @@ y_score = model.predict_proba(X_test)[:, 1]  # probability of class 1
 # y_score: shape (n_samples,), predicted probabilities for the positive class
 auc = roc_auc_score(y_test, y_score)
 print("ROC-AUC:", auc)
+
+# Finding the Precision@Top10% (K = 10%)
+n = len(X_test)
+K = max(1, int(np.ceil(0.10 * n)))
+
+idx_sorted = np.argsort(-np.asarray(y_score))
+top_k_true = np.sum(np.asarray(y_test)[idx_sorted[:K]] == 1)
+precision_topk = top_k_true / K
+
+print("Precision@Top10%:", precision_topk)

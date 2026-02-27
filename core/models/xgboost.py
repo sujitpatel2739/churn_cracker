6be676d2bd99b43b964d6451e35c7cc6d94e2766
Sujit Patel <202501987+sujitpatel2739@users.noreply.github.com>
2026-02-27 23:30:01 +0530

    Fixed xgboost.py, corrected model initialization

--- core/models/xgboost.py
+++ core/models/xgboost.py
@@ -32,15 +32,15 @@ model = XGBClassifier(
     subsample=0.8,
     colsample_bytree=0.8,
     objective='binary:logistic',
-    eval_metric='logloss',
-    random_state=42
+    random_state=42,
+    early_stopping_rounds=20,
+    eval_metric='logloss'
 )
 
 model.fit(
     X_train,
     y_train,
     eval_set=[(X_test, y_test)],
-    early_stopping_rounds=20,
     verbose=False
 )
 
